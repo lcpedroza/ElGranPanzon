ddd268c6797ab8db2524d83e5dc4727af8338583
Luis Carlos Pedroza Pineda <luisk301374@gmail.com>
2020-03-11 02:04:16 -0500

    Se insertaron los permisos. Se insertaron los permisos por roles. Se insertó in empleado de rol Cajero. Se agregó el item de Realizar pedido. Se agragó la clase EmpleadoComplemento. Se agragaron las funciones VerificarPermiso y VerificarCualquierPermiso. Se agregó la clase PermisoComplemento

---
+++ DataAccess/Tablas/EmpleadoComplemento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Tablas {
+    public partial class Empleado {
+        public bool VerificarPermiso(string codigoPermiso) {
+            foreach (var p in ROLES.PERMISOS) {
+                if (p.Codigo == codigoPermiso) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool VerificarCualquierPermiso(params string[] codigos) {
+            foreach (var c in codigos) {
+                if (VerificarPermiso(c)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ DataAccess/Tablas/PermisoComplemento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Tablas {
+    public partial class Permiso {
+        public static readonly string CrearEmpleado = "CREAR_EMPLEADO";
+        public static readonly string EditarEmpleado = "EDITAR_EMPLEADO";
+        public static readonly string EliminarEmpleado = "ELIMINAR_EMPLEADO";
+        public static readonly string VerEmpleado = "VER_EMPLEADO";
+        public static readonly string AgregarInsumo = "AGREGAR_INSUMO";
+        public static readonly string VerInventarioInsumo = "VER_INVENTARIO_I";
+        public static readonly string verInventarioComida = "VER_INVENTARIO_C";
+        public static readonly string EntregarInsumo = "ENTREGAR_INSUMO";
+        public static readonly string RealizarCompra = "REALIZAR_VENTA";
+    }
+}
